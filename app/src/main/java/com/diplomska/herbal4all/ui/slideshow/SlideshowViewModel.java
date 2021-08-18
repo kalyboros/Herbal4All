2a80f2e832dbce625dfa81e6abac119a04d67e12
kalyb <dadosa123@gmail.com>
2021-08-18 21:23:18 +0200

    Removan custom gumb in izbris nepotrebnih datotek.

---
+++ app/src/main/java/com/diplomska/herbal4all/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.diplomska.herbal4all.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
